aa5782ed37243a9e7fc1abb1e4ecf9212bd8c495
Nelson Yaccuzzi <nelsonyaccuzzi@gmail.com>
2020-05-01 23:57:07 +0000

    change version var name

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ "$TRAVIS_PULL_REQUEST" = "true"  ] || [ "$TRAVIS_BRANCH" != "master"  ]; then
-  docker buildx build --progress plain --build-arg VERSION=$VERSION --platform=linux/arm/v7 .
+  docker buildx build --progress plain --build-arg SW_VERSION=$SW_VERSION --platform=linux/arm/v7 .
   exit $?
 fi
 echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin &> /dev/null
-docker buildx build --progress plain --build-arg VERSION=$VERSION --platform=linux/arm/v7 -t $TRAVIS_REPO_SLUG:$VERSION --push .
+docker buildx build --progress plain --build-arg SW_VERSION=$SW_VERSION --platform=linux/arm/v7 -t $TRAVIS_REPO_SLUG:$SW_VERSION --push .
