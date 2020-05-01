f9214297d8ac4d7168e9b08eaa74cb39011bbcc7
Nelson Yaccuzzi <nelsonyaccuzzi@gmail.com>
2020-05-01 23:21:18 +0000

    add tag

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ "$TRAVIS_PULL_REQUEST" = "true"  ] || [ "$TRAVIS_BRANCH" != "master"  ]; then
-      docker buildx build --progress plain --platform=linux/arm/v7 .
-        exit $?
+  docker buildx build --progress plain --build-arg BAZARR_VERSION=$TRAVIS_TAG --platform=linux/arm/v7 .
+  exit $?
 fi
 echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin &> /dev/null
 TAG="${TRAVIS_TAG:-latest}"
-docker buildx build --progress plain --platform=linux/arm/v7 -t $DOCKER_REPO:$TAG --push .
+docker buildx build --progress plain --build-arg BAZARR_VERSION=$TRAVIS_TAG ---platform=linux/arm/v7 -t $DOCKER_REPO:$TAG --push .
